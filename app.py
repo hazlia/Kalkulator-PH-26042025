2a98c81698da9e22ab75a11d5351f807eb66294f
hazlia <hazliamulya8@gmail.com>
2025-04-25 22:33:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
 import streamlit as st
 import math
 
-_LOREM_IPSUM = """
-Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
-incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-"""
-
-def stream_data():
-    for word in _LOREM_IPSUM.split(" "):
-        yield word + " "
-        time.sleep(0.02)
-
-    yield pd.DataFrame(
-        np.random.randn(5, 10),
-        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-    )
-
-    for word in _LOREM_IPSUM.split(" "):
-        yield word + " "
-        time.sleep(0.02)
-
-
-if st.button("Stream data"):
-    st.write_stream(stream_data)
 # st.set_page_config(page_title="Kalkulator pH & pOH", layout="centered")
 
 # # Title
 # st.title("🧪 Kalkulator pH & pOH Larutan")
+# st.hearder("🧪 Kalkulator pH & pOH Larutan")
+# st.subhearder("🧪 Kalkulator pH & pOH Larutan")
+# st.markdown("🧪 Kalkulator pH & pOH Larutan")
 
 # # Description
 # st.write("""
